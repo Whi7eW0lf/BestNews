c39568441035f0c3c9ab60cc83116ac90ffc69d3
Slavi <slaveizlatarev@gmail.com>
2020-05-14 21:49:33 +0300

    Make project...

---
+++ src/main/java/news/SpringSiteForNewsApplication.java
@@ -0,0 +1,13 @@
+package news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSiteForNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSiteForNewsApplication.class, args);
+    }
+
+}
