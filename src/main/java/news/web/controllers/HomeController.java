9443c753eed243e4c18c43d722e639a7890d196d
Slavi <slaveizlatarev@gmail.com>
2020-05-15 22:38:39 +0300

    Edit html's ...

--- src/main/java/news/web/controllers/HomeController.java
+++ src/main/java/news/web/controllers/HomeController.java
@@ -4,15 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public ModelAndView getIndex(){
+    public ModelAndView getIndex(HttpServletRequest request){
+        System.out.println("User IP"+request.getRemoteAddr());
         return new ModelAndView("index");
     }
     @GetMapping("/news")
-    public ModelAndView getNews(){
+    public ModelAndView getNews(HttpServletRequest request){
+        System.out.println("User IP"+request.getRemoteAddr());
         return new ModelAndView("news");
+    }
+    @GetMapping("/contact")
+    public ModelAndView getContact(HttpServletRequest request){
+        System.out.println("User IP"+request.getRemoteAddr());
+        return new ModelAndView("contact");
     }
 }
