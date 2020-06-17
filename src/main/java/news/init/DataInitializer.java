e675f262381983b6efefe42eade803f48034dc3f
Slavi <slaveizlatarev@gmail.com>
2020-06-17 20:34:43 +0300

    Added data filling from json in database ( not finished )...

--- src/main/java/news/init/DataInitializer.java
+++ src/main/java/news/init/DataInitializer.java
@@ -14,86 +14,48 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.Random;
+import java.util.Arrays;
 
 @Component
 public class DataInitializer implements CommandLineRunner {
 
-    private final UserRepository userRepository; //TODO: Service
-    private final TagRepository tagRepository; //TODO: Service
-    private final CategoryRepository categoryRepository; //TODO: Service
-    private final NewsRepository newsRepository; //TODO: Service
-    private User user;
-    private Tag tag;
-    private Category category;
-    private News news;
+    private final UserService userService;
+    private final TagService tagService;
+    private final CategoryService categoryService;
+    private final NewsService newsService;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public DataInitializer(UserRepository userRepository, TagRepository tagRepository, CategoryRepository categoryRepository, NewsRepository newsRepository) {
-        this.userRepository = userRepository;
-        this.tagRepository = tagRepository;
-        this.categoryRepository = categoryRepository;
-        this.newsRepository = newsRepository;
-        this.user = new User();
-        this.tag = new Tag();
-        this.category = new Category();
-        this.news = new News();
+    public DataInitializer(UserService userService, TagService tagService, CategoryService categoryService, NewsService newsService, ModelMapper modelMapper) {
+        this.userService = userService;
+        this.tagService = tagService;
+        this.categoryService = categoryService;
+        this.newsService = newsService;
+        this.modelMapper = modelMapper;
     }
 
-    public void addNews(){
-        this.news.setImageUrl("/images/test-image-1000x466.jpg");
-        this.news.setDate(LocalDateTime.now());
-        this.news.setCategory(this.category);
-        this.news.setUser(this.user);
-        Random random = new Random();
-        this.news.setViews(Math.abs(random.nextInt()));
-        this.news.setTextNews("NULL");
-        this.news.setTitle("Test database text");
-        this.news.setTags(Collections.singletonList(this.tag));
-        this.newsRepository.saveAndFlush(this.news);
-    }
-
-    public void addCategory(){
-        this.category.setType("Random");
-        this.categoryRepository.saveAndFlush(this.category);
-
-    }
-
-    public void addTag(){
-        this.tag.setTag("TestTag");
-        this.tagRepository.saveAndFlush(tag);
-    }
-
-    public void addUser(){
-
-
-        this.user.setFirstName("Salih");
-        this.user.setLastName("Musov");
-        this.user.setIp("11.11.11.11");
-        this.user.setRole(Role.ADMINISTRATOR);
-        this.userRepository.saveAndFlush(user);
-    }
 
     @Override
     public void run(String... args) throws Exception {
 
-        if (this.categoryRepository.count()<1
-                &&this.newsRepository.count()<1
-                &&this.tagRepository.count()<1
-                &&this.userRepository.count()<1){
-            this.addCategory();
+        JsonParser parser = new JsonParser();
 
-            for (int i = 0; i < 10; i++) {
-                this.user = new User();
-                this.tag = new Tag();
-                this.news = new News();
+        if (this.categoryService.getCategoryCount()==0) {
+            CategoryDto[] categoryDto = parser.objectFromFile(CategoryDto[].class,"src/main/java/news/init/jsons/categories.json");
 
-                this.addUser();
-                this.addTag();
-                this.addNews();
-            }
+            Arrays.stream(categoryDto)
+                    .map(c->this.modelMapper.map(c, CategoryServiceModel.class))
+                    .forEach(this.categoryService::saveCategory);
         }
+
+        if (this.tagService.getTagCount()==0){
+            TagDto[] tagDto = parser.objectFromFile(TagDto[].class,"src/main/java/news/init/jsons/tags.json");
+
+            Arrays.stream(tagDto)
+                    .map(t->this.modelMapper.map(t, TagServiceModel.class))
+                    .forEach(this.tagService::saveTag);
+        }
+
+        System.out.println();
     }
 }
